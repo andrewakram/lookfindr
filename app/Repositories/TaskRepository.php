b98fa589a761b563bb95408ccefc3f2ef1e8a550
andrewakram <andrewalbert93501@gmail.com>
2025-05-18 00:21:12 +0300

    task_logs

--- app/Repositories/TaskRepository.php
+++ app/Repositories/TaskRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 use App\Events\TaskAssigned;
 use App\Interfaces\TaskRepositoryInterface;
 use App\Models\Task;
+use App\Models\TaskLog;
 
 class TaskRepository implements TaskRepositoryInterface {
 
@@ -15,20 +16,43 @@ class TaskRepository implements TaskRepositoryInterface {
 
     public function createTask($projectId,$data)
     {
-        return Task::create([
+        \DB::beginTransaction();
+
+        $task = Task::create([
             'project_id'    => $projectId,
             'title'         => $data['title'],
             'status'        => empty($data['status']) ? 'todo' : $data['status'],
             'due_date'      => empty($data['due_date']) ? now()->addHours(24) : $data['due_date'],
         ]);
 
+        TaskLog::create([
+            'user_id'   => auth()->user()->id,
+            'task_id'   => $task->id,
+            'old_value' => null,
+            'new_value' => json_encode($task),
+        ]);
+
+        \DB::commit();
+
+        return $task;
+
     }
 
     public function assignTask($task, $data)
     {
+        \DB::beginTransaction();
+
         $task->update(['user_id' => $data['member_id']]);
 
+        TaskLog::create([
+            'user_id'   => auth()->user()->id,
+            'task_id'   => $task->id,
+            'old_value' => json_encode($task),
+            'new_value' => json_encode($this->findTask($task->id)),
+        ]);
         event(new TaskAssigned($task));
+
+        \DB::commit();
 
         return $task;
     }
---
+++ database/migrations/2025_05_17_205824_create_task_logs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Task;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('task_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Task::class);
+            $table->json('old_value')->nullable();
+            $table->json('new_value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('task_logs');
+    }
+};
